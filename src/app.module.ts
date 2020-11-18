6150ca92e726ef90ebc33ab8f9d141a08bfb2743
KihoonLim <khim@playauto.co.kr>
2020-11-18 11:06:11 +0900

    one

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module,NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middieware/logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude()
+      .forRoutes('cats');
+  }
+}
